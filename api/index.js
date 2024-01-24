10cfb1227c6ec212292e67bca9596fdfe64997fe
Nebiyu <nebiyu.dessiean@gmail.com>
2024-01-24 20:42:40 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const dbClient = new MongoClient(process.env.MONGO);
+
+try {
+  const database = dbClient.db("mern-estate");
+} finally {
+  await dbClient.close();
+}
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
